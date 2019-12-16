6e09308108759e6dbaa9f8bd7ee7ccad5608d427
to4my <to4my@ya.ru>
2019-12-16 10:40:30 +0300

    prepare to deploy heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
+
+  before_action :set_current_user, except: [:show]
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -9,7 +12,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to @user, notice: I18n.t('controllers.users.updated')
     else
       render :edit
     end
@@ -17,11 +20,10 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
+  def set_current_user
+    @user = current_user
   end
 
-  # Пропишем, что разрешено передавать в params
   def user_params
     params.require(:user).permit(:name, :email)
   end
--- db/migrate/20191214125503_add_devise_to_users.rb
+++ db/migrate/20191214125503_add_devise_to_users.rb
@@ -1,17 +1,24 @@
 class AddDeviseToUsers < ActiveRecord::Migration[6.0]
   def self.up
+
     change_table :users do |t|
-      t.string :email,              null: false, default: ""
+
+      t.change :email, :string, null: false, default: "" # меняем таблицу email - новые ограничения
       t.string :encrypted_password, null: false, default: ""
 
-      t.string   :reset_password_token
+      t.string :reset_password_token
       t.datetime :reset_password_sent_at
 
       t.datetime :remember_created_at
 
+      t.integer :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
   end
 
