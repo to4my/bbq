12abe3cafad73108f6827c183dc54e24212c1e3e
to4my <to4my@ya.ru>
2019-12-19 13:58:02 +0300

    added comments model&controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 35}
 
