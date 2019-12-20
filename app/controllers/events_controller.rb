6e2f7f743ba881cd5de662d376798ddeaeed7674
to4my <to4my@ya.ru>
2019-12-20 12:15:37 +0300

    fixed subscription controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,8 @@ class EventsController < ApplicationController
   end
 
   def show
+    @new_comment = @event.comments.build(params[:comment])
+    @new_subscription = @event.subscriptions.build(params[:subscription])
   end
 
   def new
