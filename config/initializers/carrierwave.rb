3acf76f260d59f19f5131354b2f888b20b5d3765
MikeP <to4my@ya.ru>
2019-12-28 23:45:29 +0300

    carrierwave some

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
-
+    config.cache_dir     = "#{Rails.root}/tmp/uploads"
     config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_080318) do
     t.string "user_name"
     t.string "user_email"
     t.integer "event_id", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_subscriptions_on_event_id"
@@ -64,11 +64,6 @@ ActiveRecord::Schema.define(version: 2019_12_26_080318) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
