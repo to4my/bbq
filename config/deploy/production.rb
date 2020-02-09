033b427ea3b9374a9da08c0740a021c9095b6b21
to4my <to4my@ya.ru>
2020-02-09 11:19:46 +0300

    production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'apxangelskoe.ru', user: 'deploy', roles: %w{app db web}
+server 'apxangelskoe.ru', user: 'deploy', roles: %w[app db web]
 
 
 
