81b2314b26817ce605f2ac5ee440d5e6bd48436d
MikeP <to4my@ya.ru>
2020-02-13 16:54:01 +0300

    fix policies

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,6 @@ class EventsController < ApplicationController
   end
 
   def show
-    # authorize @event
     @new_comment = @event.comments.build(params[:comment])
     @new_subscription = @event.subscriptions.build(params[:subscription])
     # Болванка модели для формы добавления фотографии
@@ -22,6 +21,7 @@ class EventsController < ApplicationController
 
   def new
     @event = current_user.events.build
+    authorize @event
   end
 
   def edit
@@ -40,8 +40,9 @@ class EventsController < ApplicationController
   end
 
   def update
+    authorize @event
+
     if @event.update(event_params)
-      authorize @event
       redirect_to @event, notice: t('controllers.events.updated')
     else
       render :edit
@@ -49,16 +50,13 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event.destroy
     authorize @event
+
+    @event.destroy
     redirect_to events_url, notice: t('controllers.events.destroyed')
   end
 
   private
-
-  # def set_current_user_event
-  #   @event = current_user.events.find(params[:id])
-  # end
 
   def set_event
     @event = Event.find(params[:id])
