d9bfee6a1efdd10f95bffff6f83ff0a6dd1e12cc
to4my <to4my@ya.ru>
2020-02-18 10:09:33 +0300

    resque -> async

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.active_job.queue_adapter = :resque
+  config.active_job.queue_adapter = :async
   config.active_job.queue_name_prefix = "bbq_#{Rails.env}"
 
   # Do not eager load code on boot.
